d3f2d2685c32e1a2ce540f3e1633a2f629dab377
Gevorg <gbaghdasaryan1998@gmail.com>
2025-06-23 02:44:38 +0400

    final committ

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .setTitle('booking system')
     .setDescription('Test task')
